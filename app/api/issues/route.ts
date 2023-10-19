b548595dfae1b6ffbf991d3a4200aa95db54613c
israel <akinleyeisrael12@gmail.com>
2023-10-18 20:33:47 -0400

    initial

---
+++ app/api/issues/route.ts
@@ -0,0 +1,17 @@
+
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { createIssueSchema } from "../../valiadationSchemas";
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = createIssueSchema.safeParse(body)
+    if (!validation.success)
+        return NextResponse.json(validation.error.format, { status: 400 })
+
+    const newIssue = await prisma.issue.create({
+        data: { title: body.title, description: body.description }
+
+    });
+    return NextResponse.json(newIssue, { status: 201 })
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+export default function Home() {
+  return (
+    <div>Hello world</div>
+  )
+}
